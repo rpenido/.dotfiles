8ca2ce3a71f7700bde2752745972b9878a6d1fb4
Rômulo Penido <romulo.penido@dashsolutions.com.br>
2022-12-05 13:35:21 -0300

    Initial commit

---
+++ dot_config/nvim/after/plugin/color.lua
@@ -0,0 +1,5 @@
+vim.opt.background = 'dark'
+
+-- vim.cmd('colorscheme onedark')
+-- vim.cmd('colorscheme base16-default-dark')
+vim.cmd('colorscheme nord')
---
+++ dot_config/nvim/lua/rpenido/remap.lua
@@ -0,0 +1,3 @@
+local nnoremap = require('rpenido.keymap').nnoremap
+
+nnoremap('<leader>pv', '<cmd>Ex<CR>')
